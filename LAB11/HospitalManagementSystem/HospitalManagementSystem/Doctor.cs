aa66384670be545dd8f294e2c2cce0cc65e9e5f1
sakib <sakibhossain323@gmail.com>
2023-04-06 10:39:05 +0600

    Created Project

---
+++ LAB11/HospitalManagementSystem/HospitalManagementSystem/Doctor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem
+{
+    public class Doctor: Employee
+    {
+        public Doctor(string name, string address, string phoneNum, string email, DateTime hireDate, string dept, string pos): base(name, address, phoneNum, email, hireDate)
+        {
+            this.Department= dept;
+            this.Position= pos;
+        }
+    }
+}
---
+++ LAB11/HospitalManagementSystem/HospitalManagementSystem/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem
+{
+    public abstract class Employee
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNum { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public DateTime HireDate { get; set; }
+
+        public Employee(string name, string address, string phoneNum, string email, DateTime hireDate)
+        {
+            this.Name = name;
+            this.Address = address;
+            this.PhoneNum = phoneNum;
+            this.Email = email;
+            this.HireDate = hireDate;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine($"EmployeeDetails");
+        }
+
+        public int CalculateYearsOfService() { return DateTime.Now.Year - this.HireDate.Year; }
+    }
+}
