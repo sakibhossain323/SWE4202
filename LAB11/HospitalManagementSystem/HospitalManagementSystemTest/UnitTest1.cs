5dcf883314409225af8cffb3bd2f4dd55ef52f2e
sakib <sakibhossain323@gmail.com>
2023-04-06 09:36:12 +0600

    Created Project Files

---
+++ LAB11/HospitalManagementSystem/HospitalManagementSystemTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace HospitalManagementSystemTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
